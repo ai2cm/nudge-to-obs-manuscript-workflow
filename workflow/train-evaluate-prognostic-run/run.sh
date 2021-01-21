a0ca4dc5a4f1f3efbffdc09c238f73df2610231f
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-01-21 22:51:00 +0000

    Add test_times.json and train_times.json

--- workflow/train-evaluate-prognostic-run/run.sh
+++ workflow/train-evaluate-prognostic-run/run.sh
@@ -7,13 +7,8 @@ EXPERIMENT=2020-10-30-nudge-to-obs-GRL-paper/$NAME
 
 if [[ "$NAME" == "rf-control" ]]; then
     TRAINING_CONFIG="training-config.yaml"
-    TRAINING_TIMES="../../train.json"
 elif [[ "$NAME" == "rf-dQ1-dQ2-only" ]]; then
     TRAINING_CONFIG="training-config-dQ1-dQ2-only.yaml"
-    TRAINING_TIMES="../../train.json"
-elif [[ "$NAME" == "rf-august-data" ]]; then
-    TRAINING_CONFIG="training-config.yaml"
-    TRAINING_TIMES="../../train_august.json"
 else
     echo "Configuration not defined for ${NAME}"
     exit 1
@@ -27,8 +22,8 @@ argo submit \
     -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
     -p initial-condition="20160101.000000" \
     -p prognostic-run-config="$(< prognostic-run.yaml)" \
-    -p train-times="$(<  ${TRAINING_TIMES})" \
-    -p test-times="$(<  ../../test.json)" \
+    -p train-times="$(<  train_times.json)" \
+    -p test-times="$(<  test_times.json)" \
     -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT \
     -p segment-count=12 \
     -p cpu-prog=6 \
