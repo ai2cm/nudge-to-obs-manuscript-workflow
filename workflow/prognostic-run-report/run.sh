876823df543d811ed683cbe0633e09306895721c
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-03-26 16:58:42 +0000

    Update prongostic run report submission script

--- workflow/prognostic-run-report/run.sh
+++ workflow/prognostic-run-report/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#name=2021-03-15-n2o-weather-forecast
-#argo submit --from=workflowtemplate/prognostic-run-diags \
-#    --name $name \
-#    -p runs="$(< rundirs_weather.json)" \
-#    -p flags="--verification nudged_c48_fv3gfs_2015_2016"
+
+name=2021-03-15-n2o-weather-forecast
+argo submit --from=workflowtemplate/prognostic-run-diags \
+    --name $name \
+    -p runs="$(< rundirs_weather.json)" \
+    -p flags="--verification nudged_c48_fv3gfs_2015_2016"
 
 
 name=2021-03-15-n2o-climate-runs
@@ -12,9 +13,3 @@ argo submit --from=workflowtemplate/prognostic-run-diags \
     -p runs="$(< rundirs_climate.json)" \
     -p flags="--verification nudged_c48_fv3gfs_2015_2016"
 
-
-#name=2020-11-10-n2o-compare-to-c96
-#argo submit --from=workflowtemplate/prognostic-run-diags \
-#    --name $name \
-#    -p runs="$(< rundirs_compare_to_c96.json)" \
-#    -p flags="--verification nudged_c48_fv3gfs_2015_2016"
